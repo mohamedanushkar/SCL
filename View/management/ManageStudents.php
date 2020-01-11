fd62f00c242f9a78e0a612911b5a0fa307dfae4e
mohamedanushkar <mohamedanushkar@gmail.com>
2020-01-11 17:39:26 +0530

    finalise Student UI

--- View/management/ManageStudents.php
+++ View/management/ManageStudents.php
@@ -9,12 +9,13 @@
         <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.1.1/dt-1.10.20/r-2.2.3/datatables.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+        <link rel="stylesheet" href="./../../Assets/CSS/Main.css">
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     </head>
     <body style="margin: 20px">
+    <p class="CenterTopic">Student Management</p>
         <form  id="insert_data" class="col-md-5" >
 
             <div class="form-group">
